c953e8a444b8f487caf6467185cddfa53ef026bd
Mozibix <opuzmoses@gmail.com>
2025-12-01 18:24:57 +0100

    chores: updated

--- App.tsx
+++ App.tsx
@@ -56,7 +56,7 @@ const App = () => {
   const shakeAnim = useState(new Animated.Value(0))[0]; // Horizontal (X)
   const bounceAnim = useState(new Animated.Value(0))[0]; // Vertical (Y)
 
-  // Calculator handlers
+  // Calculator handlerss
   const handleNumber = (num: string) => {
     setCurrValue(currValue === '0' ? num : currValue + num);
   };
